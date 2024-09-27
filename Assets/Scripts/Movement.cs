55c64759326b7a418e903bf47ef86565b0c483f4
SilverString5 <siennapinewillow@gmail.com>
2024-09-27 12:04:55 +1000

    2nd last movement commit
    Logic works, just need to fix speed and locations :)

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private GameObject pacStud;
+    [SerializeField] private Animator pacAnim;
+    private Tweener tweener;
+    private Vector3 location1;
+    private Vector3 location2;
+    private Vector3 location3;
+    private Vector3 location4;
+    private float counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        location1 = new Vector3(-4.57f, 8.44f, -1.0f);
+        location2 = new Vector3(0.42f, 8.44f, -1.0f);
+        location3 = new Vector3(0.42f, 1.42f, -1.0f);
+        location4 = new Vector3(-4.57f, 1.42f, -1.0f);
+        //pacStud = new GameObject();
+        tweener = GameObject.FindFirstObjectByType<Tweener>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pacStud.transform.position == location1)
+        {
+            pacAnim.SetInteger("Param", 0);
+            tweener.AddTween(pacStud.transform, pacStud.transform.position, location2, 0.5f);
+        }
+
+        if (pacStud.transform.position == location2)
+        {
+            pacAnim.SetInteger("Param", 1);
+            tweener.AddTween(pacStud.transform, pacStud.transform.position, location3, 0.5f);
+        }
+
+        if (pacStud.transform.position == location3)
+        {
+            pacAnim.SetInteger("Param", 2);
+            tweener.AddTween(pacStud.transform, pacStud.transform.position, location4, 0.5f);
+        }
+
+
+        if (pacStud.transform.position == location4)
+        {
+            tweener.AddTween(pacStud.transform, pacStud.transform.position, location1, 0.5f);
+            pacAnim.SetInteger("Param", 3);
+        } 
+            
+
+
+
+    }
+}
---
+++ Assets/Scripts/Tweener.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tweener : MonoBehaviour
+{
+    //private Tween activeTween;
+    private List<Tween> activeTweens;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("THis ran brahj");
+        activeTweens = new List<Tween>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<Tween> matches = new List<Tween>();
+        foreach (Tween tween in activeTweens)
+        {
+            if (tween != null)
+            {
+                if (Vector3.Distance(tween.Target.position, tween.EndPos) > 0.1f)
+                {
+                    //Time.time - StartPos
+                    float timeFraction = (Time.time - tween.StartTime) / tween.Duration;
+                    timeFraction = Mathf.Pow(timeFraction, 3);
+                    tween.Target.position = Vector3.Lerp(tween.StartPos, tween.EndPos, timeFraction);
+                }
+                else
+                {
+                    tween.Target.position = tween.EndPos;
+                    matches.Add(tween);
+                    //activeTween = null;
+                    
+                    //activeTweens.Remove(tween);
+                }
+            }
+            
+        }
+        foreach (Tween match in matches)
+        {
+            activeTweens.Remove(match);
+        }
+
+        
+        
+        
+    }
+
+    public bool AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
+    {
+        if (!TweenExists(targetObject))
+        {
+            Debug.Log("Adding new tween");
+            Tween tween = new Tween(targetObject, startPos, endPos, Time.time, duration);
+            activeTweens.Add(tween);
+            return true;
+        }
+        return false;
+    }
+
+    public bool TweenExists(Transform target)
+    {
+        foreach (Tween tween in activeTweens)
+        {
+            if (tween.Target == target)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
